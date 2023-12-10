756167ac2f392c130846f0306bebcd10f1c78986
Veronika Timakova <tymnik@ukr.net>
2023-12-11 01:23:59 +0300

    feat: About.jsx, styles, images

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,55 @@
+// import Button from '../Button/Button';
+// import { Link } from "react-router-dom";
+import aboutFirst from "../../assets/About/about-img-1.webp";
+import aboutSecond from "../../assets/About/about-img-2.webp";
+import styles from "./About.module.css";
+
 const About = () => {
-	return <div>About</div>;
+  return (
+    <section id="about" className={styles.about}>
+      <h2 className={styles.aboutTitle}>Про бригаду</h2>
+      <div className={styles.aboutWrapper}>
+        <div className={styles.aboutFirstBlock}>
+          <img
+            src={aboutFirst}
+            alt="Brigade photo"
+            className={styles.aboutFirstImage}
+          />
+          <div className={styles.aboutFirstBlockText}>
+            <p className={styles.aboutText}>
+              З самого початку повномасштабного вторгнення агресора в Україну 9й
+              полк оперативного призначення НГУ імені Богдана Завади став на
+              захист рубежів нашого регіону та брали участь в обороні населених
+              пунктів Мелітополь, Молочанськ, Токмак, Кам'янське, Мала Токмачка.{" "}
+            </p>
+            <p className={styles.aboutText}>
+              Стійкість та мужність цих воїнів дала змогу зупинити переважаючі
+              сили ворога на напрямках Василівка - Запоріжжя та Токмак - Оріхів.{" "}
+            </p>
+          </div>
+        </div>
+        <div className={styles.aboutSecondBlock}>
+          <img
+            src={aboutSecond}
+            alt="Brigade photo"
+            className={styles.aboutSecondImage}
+          />
+          <div className={styles.aboutSecondBlock}>
+            <p className={styles.aboutText}>
+              В січні 2023 року 9й полк було реорганізовано у 15 бригаду
+              оперативного призначення імені Богдана Завади, яка увійшла у список
+              бригад "Гвардії наступу".
+            </p>
+            <p className={styles.aboutText}>
+              Зараз бійці бригади мужньо та стійко стримують навалу агресора й
+              крок за кроком звільняють території наших рідних земель.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <Link to=''><Button/></Link> */}
+    </section>
+  );
 };
 
 export default About;
