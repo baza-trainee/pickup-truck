767deda1e5d146f62baa7ed1ea8b3647f0e701d7
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-12-12 00:03:33 +0200

    adds component Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,34 @@
-const Modal = () => {
-	return <div>Modal</div>;
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+
+import { IoCloseOutline } from "react-icons/io5";
+
+import styles from "./Modal.module.css";
+
+const modalRoot = document.querySelector("#modal-root");
+
+const Modal = ({ close, children }) => {
+  const handleClose = ({ target, currentTarget, code }) => {
+    if (target === currentTarget || code === "Escape") {
+      close();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", handleClose);
+
+    return () => document.body.removeEventListener("keydown", handleClose);
+  });
+
+  return createPortal(
+    <div className={styles.overlay} onClick={handleClose}>
+      <div className={styles.modal}>
+        <IoCloseOutline className={styles.btnClose} onClick={close} size={24} />
+        {children}
+      </div>
+    </div>,
+    modalRoot
+  );
 };
 
 export default Modal;
