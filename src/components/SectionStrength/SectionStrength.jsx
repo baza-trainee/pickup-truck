23c381743d639c38c24e4e57a4386977aac1720b
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-12-12 00:05:22 +0200

    changes styles components ScrollToTop, SectionStrength

--- src/components/SectionStrength/SectionStrength.jsx
+++ src/components/SectionStrength/SectionStrength.jsx
@@ -1,20 +1,43 @@
+import { useState } from "react";
 import { GoShareAndroid } from "react-icons/go";
+
 import Container from "../Container/Container";
 
 import styles from "./SectionStrength.module.css";
+import Modal from "../Modal/Modal";
+import ModalShare from "../ModalShare/ModalShare";
 
 const SectionStrength = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    if (showModal) {
+      document.body.classList.remove("modal-open");
+    } else {
+      document.body.classList.add("modal-open");
+    }
+    setShowModal(!showModal);
+  };
+
   return (
-    <section className={styles.section}>
-      <Container>
-        <h2 className={styles.title}>В єданні - сила!</h2>
-        <h3 className={styles.subTitle}>Розповідай друзям про збір</h3>
-        <button className={styles.btn} type="button">
-          Поділитися
-          <GoShareAndroid size={24} />
-        </button>
-      </Container>
-    </section>
+    <>
+      <section className={styles.section}>
+        <Container>
+          <h2 className={styles.title}>В єданні - сила!</h2>
+          <h3 className={styles.subTitle}>Розповідай друзям про збір</h3>
+          <button className={styles.btn} type="button" onClick={toggleModal}>
+            Поділитися
+            <GoShareAndroid size={24} />
+          </button>
+        </Container>
+      </section>
+
+      {showModal && (
+        <Modal close={toggleModal}>
+          <ModalShare close={toggleModal} />
+        </Modal>
+      )}
+    </>
   );
 };
 export default SectionStrength;
