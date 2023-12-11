62bca0c2dd161c987bba4b2b1197616ba2fcc427
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-12-11 15:43:54 +0200

    adds component SectionStrength

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import About from './components/About/About';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Important from './components/Important/Important';
-import ProgressBar from './components/ProgressBar/ProgressBar';
+import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Important from "./components/Important/Important";
+import ProgressBar from "./components/ProgressBar/ProgressBar";
+import SectionStrength from "./components/SectionStrength/SectionStrength";
 
 const App = () => {
-	return (
-		<>
-			<Header />
-			<Hero />
-			<ProgressBar />
-			<About />
-			<Important />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Hero />
+      <ProgressBar />
+      <About />
+      <Important />
+      <SectionStrength />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
