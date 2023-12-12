b9d41dfffd6464fc052978d28decfccdfcd976b8
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-12-12 22:23:57 +0200

    adds component Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,41 @@
-const Button = () => {
-	return <div>Button</div>;
+import { useState } from "react";
+
+import Modal from "../Modal/Modal";
+import ModalShare from "../ModalShare/ModalShare";
+import ModalThanks from "../ModalThanks/ModalThanks";
+
+import styles from "./Button.module.css";
+
+const Button = ({ isShare, children }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    if (showModal) {
+      document.body.classList.remove("modal-open");
+    } else {
+      document.body.classList.add("modal-open");
+    }
+
+    setShowModal(!showModal);
+  };
+
+  return (
+    <>
+      <button className={styles.btn} type="button" onClick={toggleModal}>
+        {children}
+      </button>
+
+      {showModal && (
+        <Modal close={toggleModal}>
+          {isShare ? (
+            <ModalShare close={toggleModal} />
+          ) : (
+            <ModalThanks close={toggleModal} />
+          )}
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default Button;
