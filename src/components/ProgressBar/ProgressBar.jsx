b020e0c47f0a53685c1d8f8dd770e3b76d66839b
dav1dashka <dujadavyd17@gmail.com>
2023-12-15 20:19:19 +0200

    feat: add progressBar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,5 +1,49 @@
+import { fetchAccountBalance } from '../../services/fetchAccountBalance';
+import { useQuery } from '@tanstack/react-query';
+import styles from './ProgressBar.module.css';
+
+const formatBalance = (balance = 0) => {
+	if (balance < 1000) {
+		return balance.toString();
+	} else {
+		return balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+	}
+};
+
 const ProgressBar = () => {
-	return <div>ProgressBar</div>;
+	const { data: balance } = useQuery(['accountBalance'], fetchAccountBalance, {
+	  refetchInterval: 60000,
+	});
+
+	let goal = '250 000';
+	const width = balance > 100 ? `${Math.floor((balance / 250000) * 98)}%` : '0%';
+	const formattedBalance = formatBalance(balance);
+
+	return (
+		<section id='goal' className={styles.goal}>
+			<div className={styles.wrapper}>
+				<div className={styles.title}>Готуємось разом до нашої перемоги</div>
+				<div className={styles.progress}>
+					<div className={styles.container}>
+						<div className={styles.textLeft}>
+							<div className={styles.label}><span>Вже зібрано</span></div>
+							<div className={styles.amount}><span>{`${formattedBalance || 0}`} грн</span></div>
+						</div>
+						<div className={styles.bar}>
+							<div className={styles.border}>
+								<div className={styles.fill} style={{ width: width }}></div>
+							</div>
+						</div>
+						<div className={styles.textRight}>
+							<div className={styles.label}><span>Наша мета</span></div>
+							<div className={styles.amount}><span>{`${goal || 0}`} грн</span></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section >
+	);
 };
 
 export default ProgressBar;
