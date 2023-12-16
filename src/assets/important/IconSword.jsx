659a252392d4b346e6ce4ea350bf11ef2df9229c
Valera001 <jokerr192@gmail.com>
2023-12-16 14:13:24 +0200

    feat: add important section

---
+++ src/assets/important/IconSword.jsx
@@ -0,0 +1,17 @@
+export const IconSword = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        id="Vector"
+        d="M38.389 0L29.3482 3.5L15.7576 17.36L22.7 24.44L36.2709 10.58C39.2322 7.56 38.389 0 38.389 0ZM11.5804 16L8.81518 18.78L11.5804 21.6L0.5 32.92L7.42278 40L18.5228 28.68L21.2879 31.52L24.0727 28.68L11.5804 16Z"
+        fill="#FFCC00"
+      />
+    </svg>
+  );
+};
--- src/components/Important/Important.jsx
+++ src/components/Important/Important.jsx
@@ -1,5 +1,78 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Container from "../Container/Container";
+import styles from "./Important.module.css";
+import { IconFire } from "../../assets/important/IconFire";
+import { IconSpeed } from "../../assets/important/IconSpeed";
+import { IconSword } from "../../assets/important/IconSword";
+import Slide1 from "../../assets/important/slide1.webp";
+import Slide2 from "../../assets/important/slide2.webp";
+import Slide3 from "../../assets/important/slide3.webp";
+import Slide4 from "../../assets/important/slide4.webp";
+import Slide5 from "../../assets/important/slide5.webp";
+
 const Important = () => {
-	return <div>Important</div>;
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
+  return (
+    <section className={styles.important}>
+      <h2>Чому це важливо?</h2>
+      <div className={styles.importantWrap}>
+        <Slider
+          className={styles.importantSlider}
+          т
+          infinite={settings.infinite}
+          speed={settings.speed}
+          slidesToShow={settings.slidesToShow}
+          slidesToScroll={settings.slidesToScroll}
+          autoplay={settings.autoplay}
+          autoplaySpeed={settings.autoplaySpeed}
+        >
+          <div>
+            <img src={Slide1} alt="Slide1" />
+          </div>
+          <div>
+            <img src={Slide2} alt="Slide2" />
+          </div>
+          <div>
+            <img src={Slide3} alt="Slide3" />
+          </div>
+          <div>
+            <img src={Slide4} alt="Slide4" />
+          </div>
+          <div>
+            <img src={Slide5} alt="Slide5" />
+          </div>
+        </Slider>
+        <div className={styles.importantApsolute}>
+          <Container>
+            <div className={styles.importantApsoluteWrapper}>
+              <div className={styles.importantItem}>
+                <IconFire />
+                <span>Можуть дістатися до будь-якої військової локації</span>
+              </div>
+              <div className={styles.importantItem}>
+                <IconSpeed />
+                <span>Швидко евакуювати поранених військових</span>
+              </div>
+              <div className={styles.importantItem}>
+                <IconSword />
+                <span>Доставити необхідні боєприпаси на бойові позиції</span>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Important;
